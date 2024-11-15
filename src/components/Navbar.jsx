15370a7d399c3580a6e5d340f3f6ac229279581c
ARaviteja91 <roborteja91@gmail.com>
2024-11-15 23:56:17 +0530

    Popup Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ import { Link,useParams,useLocation,useNavigate } from "react-router-dom";
 function Navbar() {
     const location = useLocation();
     const  { topics } = useParams();
+    
     const [username, setusername] = useState('')
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const n = localStorage.getItem('user')
     const navigate = useNavigate()
     // console.log(location)
@@ -28,18 +31,28 @@ function Navbar() {
 
     const handleBack=()=>{
       let param = location.pathname
-      let regex = /\/[A-Za-z]+\/([0-9]+(\/[0-9]+)+)/
-      if(regex.test(param)){
-        const confirmation = confirm("Your progress will be lost")
-        if(confirmation){
-          navigate(-1)
-        }else{
-          return
-        }
-      
+      let regex = /flashcards\/([0-9]+(\/[0-9]+)+)/
+      if(regex.test(param) == true){
+        openModal()
       }else{
         navigate(-1)
       }
+    }
+
+
+    const openModal = () => {
+      // setModalImageSrc(src);
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);       
+      // setModalImageSrc("");
+    };
+
+    const closeQuiz =()=>{
+      setIsModalOpen(false)
+      navigate(-1)
     }
   return (
     <>
@@ -52,6 +65,18 @@ function Navbar() {
           <button className='back-btn' onClick={handlelogout}  > Logout </button>
       </div>)
        }
+
+       {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-body">
+              <h3>Your Progress will be lost</h3>
+              <button onClick={closeQuiz} >Ok</button>
+              <button onClick={closeModal}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
